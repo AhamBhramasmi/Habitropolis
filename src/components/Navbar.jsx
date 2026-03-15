5b6d4da35f075d42d918d0668f97544d166adaff
anushreeh29-dev <anushreeh29@gmail.com>
2026-03-15 16:58:58 +0530

    Fix navbar routing using React Router Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,25 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav style={{
       display: "flex",
       justifyContent: "space-between",
-      alignItems: "center",
-      padding: "16px 40px",
-      backgroundColor: "#0f172a",
+      padding: "15px 20px",
+      background: "#111",
       color: "white"
     }}>
-      
       <h2>Habitropolis</h2>
 
-      <div style={{display:"flex", gap:"20px"}}>
-        <a href="#">Home</a>
-        <a href="#">Habits</a>
-        <a href="#">Stats</a>
-      </div>
+      <div style={{ display: "flex", gap: "20px" }}>
+        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+          Dashboard
+        </Link>
 
+        <Link to="/stats" style={{ color: "white", textDecoration: "none" }}>
+          Stats
+        </Link>
+      </div>
     </nav>
   );
 }
